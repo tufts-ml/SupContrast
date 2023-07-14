32b57f6378ab6915bb44c5c7fe6998368413f8f9
PatrickFeeney <patrickfeeney1225@gmail.com>
2023-07-14 14:39:44 -0400

    Split Linear Config

---
+++ config_linear/cifar10_run.sh
@@ -0,0 +1,3 @@
+cd ~/TuftsGPU/
+python grid_sbatch.py -no_label ../SupContrast/config_linear/slurm_ccgpu.toml ../SupContrast/config_linear/cifar10_hyper_search.toml
+
--- config_linear/run.sh
+++
@@ -1,3 +0,0 @@
-cd ~/TuftsGPU/
-python grid_sbatch.py -no_label ../SupContrast/config_linear/slurm_ccgpu.toml ../SupContrast/config_linear/hyper_search.toml
-
