667767212f0b5a35deaccadd28f8862b48520a41
PatrickFeeney <patrickfeeney1225@gmail.com>
2024-01-08 14:04:13 -0500

    Remove Typo

--- config_supcon/cifar10_run.sh
+++ config_supcon/cifar10_run.sh
@@ -1,2 +1,2 @@
 cd ~/TuftsGPU/
-python grid_sbatch.py -no_label ../SupContrast/config_supcon/slurm_ccgpu.toml ../SupContrast/config_supcon/cifar_10.toml
+python grid_sbatch.py -no_label ../SupContrast/config_supcon/slurm_ccgpu.toml ../SupContrast/config_supcon/cifar10.toml
