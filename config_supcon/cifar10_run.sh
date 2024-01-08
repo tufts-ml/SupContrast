90a3d4bef557a868e1cee06e01726795cf6ec42a
PatrickFeeney <patrickfeeney1225@gmail.com>
2024-01-08 14:02:55 -0500

    Fix Config File Name

--- config_supcon/cifar10_run.sh
+++ config_supcon/cifar10_run.sh
@@ -1,2 +1,2 @@
 cd ~/TuftsGPU/
-python grid_sbatch.py -no_label ../SupContrast/config_supcon/slurm_ccgpu.toml ../SupContrast/config_supcon/hyper_search.toml
+python grid_sbatch.py -no_label ../SupContrast/config_supcon/slurm_ccgpu.toml ../SupContrast/config_supcon/cifar_10.toml
