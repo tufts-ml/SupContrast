79a8df2f5ea67a69623965906018312397b1000d
PatrickFeeney <patrickfeeney1225@gmail.com>
2024-01-24 15:23:00 -0500

    Fix Test Checkpoint Bug

--- main_test_checkpoint.py
+++ main_test_checkpoint.py
@@ -12,9 +12,13 @@ if __name__ == "__main__":
     opt.valid_split = 0
 
     # edit this section to set model and dataset
-    model_loc = Path("save/SupCon/EDIT/EDIT.pth")
     opt.dataset = "EDIT"
-    opt.save_folder = model_loc.parent()
+    model_loc = Path(f"save/SupCon/{opt.dataset}_models/EDIT/EDIT.pth")
+
+    opt.save_folder = model_loc.parent
+    if opt.dataset == "imagenet100":
+        opt.data_folder = '/cluster/tufts/hugheslab/datasets/ImageNet100/train/'
+    print(opt)
 
     train_loader, _, test_loader = set_loader(opt, contrast_trans=True, for_test=True)
     model = set_model(opt).cuda()
