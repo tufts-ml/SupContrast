4bf38687af4486015665630aed1efdcf285502e8
PatrickFeeney <patrickfeeney1225@gmail.com>
2024-01-28 12:06:03 -0500

    Revise Curves and Remove Conf Mat

--- embed_stats.py
+++ embed_stats.py
@@ -2,58 +2,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import sklearn.metrics
 import torch
-from torch.nn.functional import cosine_similarity
-
-
-def cos_sim_conf_mat(embeds, labels):
-    n_labels = len(torch.unique(labels))
-    conf_mat = torch.empty((n_labels, n_labels))
-    for i in range(n_labels):
-        for j in range(i, n_labels):
-            # compute cosine similarities between pairs from classes i and j
-            i_embeds = embeds[labels == i].unsqueeze(0)
-            j_embeds = embeds[labels == j].unsqueeze(1)
-            conf_entries = cosine_similarity(i_embeds, j_embeds, dim=2)
-            if i == j:
-                # remove diagonal entries
-                conf_entries = conf_entries[~torch.eye(conf_entries.shape[0], dtype=bool)]
-            # take mean of cosine similarity
-            conf_val = torch.mean(conf_entries)
-            conf_mat[i, j] = conf_val
-            conf_mat[j, i] = conf_val
-    return conf_mat
-
-
-def plot_conf_mat(conf_mat, labels="auto"):
-    fig_folder = Path("figures/confusion")
-    fig_folder.mkdir(exist_ok=True)
-    fig, ax = plt.subplots()
-    ax = sns.heatmap(conf_mat, vmin=0.25, vmax=0.85, cmap="Blues", annot=True, fmt=".2f",
-                     square=True, ax=ax, xticklabels=labels, yticklabels=labels)
-    plt.title("SINCERE Loss" if "new" in out_folder.name else "SupCon Loss")
-    fig.savefig(fig_folder / (out_folder.name + ".pdf"), bbox_inches='tight')
-    plt.close()
-
-
-def pair_sim_mat(embeds):
-    n_embeds = embeds.shape[0]
-    pair_mat = torch.empty((n_embeds, n_embeds))
-    # break up computation for each image for less memory usage
-    for i in range(n_embeds):
-        cur_pairs = cosine_similarity(embeds[i], embeds, dim=1)
-        pair_mat[i] = cur_pairs
-    return pair_mat
-
-
-def pair_mat_to_target_noise(pair_mat, labels, target_label):
-    # get top similarity from target distribution to target distribution
-    target_mask = labels == target_label
-    target_sim = pair_mat[target_mask][:, target_mask]
-    # remove diagonal entries and flatten
-    target_sim = target_sim[~torch.eye(target_sim.shape[0], dtype=bool)]
-    # get top similarity from target distribution to noise distribution
-    noise_sim = pair_mat[target_mask][:, ~target_mask].flatten()
-    return target_sim, noise_sim
 
 
 def pair_sim_hist(pred_dict, class_labels, out_folder):
@@ -78,24 +26,25 @@ def pair_sim_hist(pred_dict, class_labels, out_folder):
         plt.close()
 
 
-def pair_sim_curves(pair_mat, labels, class_labels, out_folder):
-    roc_fig_folder = Path("figures/roc")
+def pair_sim_curves(pred_dict, class_labels, out_folder):
+    roc_fig_folder = Path("figures/roc") / out_folder.name
     roc_fig_folder.mkdir(exist_ok=True)
-    pr_fig_folder = Path("figures/pr")
+    pr_fig_folder = Path("figures/pr") / out_folder.name
     pr_fig_folder.mkdir(exist_ok=True)
-    n_labels = len(torch.unique(labels))
+    n_labels = len(torch.unique(pred_dict["target_label"]))
     for label in range(n_labels):
-        target_sim, noise_sim = pair_mat_to_target_noise(pair_mat, labels, label)
+        target_sim = pred_dict["target_sim"][pred_dict["target_label"] == label]
+        noise_sim = pred_dict["noise_sim"][pred_dict["target_label"] == label]
         # plot ROC and save
         fig, ax = plt.subplots()
         sklearn.metrics.RocCurveDisplay.from_predictions(
             [class_labels[label]] * len(target_sim) + ["Noise"] * len(noise_sim),
             torch.hstack((target_sim, noise_sim)),
             pos_label=class_labels[label],
-            name="SINCERE Loss" if "new" in out_folder.name else "SupCon Loss",
+            name="SINCERE Loss" if "SINCERE" in out_folder.name else "SupCon Loss",
             ax=ax,
         )
-        fig.savefig(roc_fig_folder / (out_folder.name + "_" + class_labels[label].lower() + ".pdf"))
+        fig.savefig(roc_fig_folder / (class_labels[label].lower() + ".pdf"))
         plt.close()
         # plot PR and save
         fig, ax = plt.subplots()
@@ -103,14 +52,15 @@ def pair_sim_curves(pair_mat, labels, class_labels, out_folder):
             [class_labels[label]] * len(target_sim) + ["Noise"] * len(noise_sim),
             torch.hstack((target_sim, noise_sim)),
             pos_label=class_labels[label],
-            name="SINCERE Loss" if "new" in out_folder.name else "SupCon Loss",
+            name="SINCERE Loss" if "SINCERE" in out_folder.name else "SupCon Loss",
             ax=ax,
         )
-        fig.savefig(pr_fig_folder / (out_folder.name + "_" + class_labels[label].lower() + ".pdf"))
+        fig.savefig(pr_fig_folder / (class_labels[label].lower() + ".pdf"))
         plt.close()
 
 
 def expected_bound(pair_mat, labels, temp=0.1):
+    # TODO revise to use train_embeds or pred_dict?
     # apply temperature to cosine similarities
     pair_mat /= temp
 
@@ -204,15 +154,5 @@ if __name__ == "__main__":
             class_labels = ('Cat', 'Dog')
         # paired similarity histogram
         pair_sim_hist(test_pred_dict, class_labels, out_folder)
-        # # paired similarity ROC and PR curves
-        # pair_sim_curves(pair_mat, labels, class_labels, out_folder)
-        # # cosine similarity confusion matrix
-        # if not (out_folder / "train_conf_mat.pth").exists():
-        #     embeds = torch.load(out_folder / "train_embeds.pth")
-        #     conf_mat = cos_sim_conf_mat(embeds, labels)
-        #     torch.save(conf_mat, out_folder / "train_conf_mat.pth")
-        # else:
-        #     conf_mat = torch.load(out_folder / "train_conf_mat.pth")
-        # print(conf_mat)
-        # plot_conf_mat(conf_mat, class_labels)
-        print()
+        # paired similarity ROC and PR curves
+        pair_sim_curves(test_pred_dict, class_labels, out_folder)
