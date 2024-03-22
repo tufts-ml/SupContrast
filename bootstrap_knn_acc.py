7d77116af86be9f91a5137055ac2480cd391b18c
PatrickFeeney <patrickfeeney1225@gmail.com>
2024-03-22 09:06:31 -0400

    KNN Acc Bootstrap for 3+ Models

--- bootstrap_knn_acc.py
+++ bootstrap_knn_acc.py
@@ -42,40 +42,53 @@ if __name__ == "__main__":
 
     from bootstrap_lin_acc import bootstrap_metric, bootstrap_dif
 
-    # out_folders should have pairs of models to compare
-    out_folders = [Path("save/SupCon/cifar10_models/SINCERE_cifar10_resnet50_lr_0.65_decay_0.0001_bsz_512_temp_0.1_trial_0_cosine_warm_2024_01_20-22_04_43/"),  # noqa: E501
-                   Path("save/SupCon/cifar10_models/SupCon_cifar10_resnet50_lr_0.35_decay_0.0001_bsz_512_temp_0.05_trial_0_cosine_warm_2024_01_19-15_04_54/"),  # noqa: E501
-                   Path("save/SupCon/cifar2_models/SINCERE_cifar2_resnet50_lr_0.65_decay_0.0001_bsz_512_temp_0.1_trial_0_cosine_warm_2024_01_22-09_32_40/"),  # noqa: E501
-                   Path("save/SupCon/cifar2_models/SupCon_cifar2_resnet50_lr_0.5_decay_0.0001_bsz_512_temp_0.1_trial_0_cosine_warm_2024_01_22-09_32_42/"),  # noqa: E501
-                   Path("save/SupCon/cifar100_models/SINCERE_cifar100_resnet50_lr_0.65_decay_0.0001_bsz_512_temp_0.05_trial_0_cosine_warm_2024_01_22-09_32_28/"),  # noqa: E501
-                   Path("save/SupCon/cifar100_models/SupCon_cifar100_resnet50_lr_0.65_decay_0.0001_bsz_512_temp_0.1_trial_0_cosine_warm_2024_01_22-09_32_31/"),  # noqa: E501
-                   Path("save/SupCon/imagenet100_models/SINCERE_imagenet100_resnet50_lr_0.65_decay_0.0001_bsz_512_temp_0.05_trial_0_cosine_warm_2024_01_22-09_32_18/"),  # noqa: E501
-                   Path("save/SupCon/imagenet100_models/SupCon_imagenet100_resnet50_lr_0.5_decay_0.0001_bsz_512_temp_0.05_trial_0_cosine_warm_2024_01_22-09_32_20/"),]  # noqa: E501
+    # model_folders_group should have lists of models to compare
+    model_folders_group = [
+        [
+            Path("save/SupCon/cifar10_models/SINCERE_cifar10_resnet50_lr_0.65_decay_0.0001_bsz_512_temp_0.1_trial_0_cosine_warm_2024_01_20-22_04_43/"),  # noqa: E501
+            Path("save/SupCon/cifar10_models/SupCon_cifar10_resnet50_lr_0.35_decay_0.0001_bsz_512_temp_0.05_trial_0_cosine_warm_2024_01_19-15_04_54/"),  # noqa: E501
+            Path("save/SupCon/cifar10_models/EpsSupInfoNCE_cifar10_resnet50_lr_0.5_decay_0.0001_bsz_512_temp_0.1_trial_0_cosine_warm_2024_03_21-12_28_30/"),  # noqa: E501
+        ],
+        [
+            Path("save/SupCon/cifar2_models/SINCERE_cifar2_resnet50_lr_0.65_decay_0.0001_bsz_512_temp_0.1_trial_0_cosine_warm_2024_01_22-09_32_40/"),  # noqa: E501
+            Path("save/SupCon/cifar2_models/SupCon_cifar2_resnet50_lr_0.5_decay_0.0001_bsz_512_temp_0.1_trial_0_cosine_warm_2024_01_22-09_32_42/"),  # noqa: E501
+            Path("save/SupCon/cifar2_models/EpsSupInfoNCE_cifar2_resnet50_lr_0.5_decay_0.0001_bsz_512_temp_0.1_trial_0_cosine_warm_2024_03_21-12_52_23/"),  # noqa: E501
+        ],
+        [
+            Path("save/SupCon/cifar100_models/SINCERE_cifar100_resnet50_lr_0.65_decay_0.0001_bsz_512_temp_0.05_trial_0_cosine_warm_2024_01_22-09_32_28/"),  # noqa: E501
+            Path("save/SupCon/cifar100_models/SupCon_cifar100_resnet50_lr_0.65_decay_0.0001_bsz_512_temp_0.1_trial_0_cosine_warm_2024_01_22-09_32_31/"),  # noqa: E501
+            Path("save/SupCon/cifar100_models/EpsSupInfoNCE_cifar100_resnet50_lr_0.5_decay_0.0001_bsz_512_temp_0.1_trial_0_cosine_warm_2024_03_21-12_52_07/"),  # noqa: E501
+        ],
+        [
+            Path("save/SupCon/imagenet100_models/SINCERE_imagenet100_resnet50_lr_0.65_decay_0.0001_bsz_512_temp_0.05_trial_0_cosine_warm_2024_01_22-09_32_18/"),  # noqa: E501
+            Path("save/SupCon/imagenet100_models/SupCon_imagenet100_resnet50_lr_0.5_decay_0.0001_bsz_512_temp_0.05_trial_0_cosine_warm_2024_01_22-09_32_20/"),  # noqa: E501
+        ],
+    ]
     for k in [1, 5]:
         print(f"{k}NN Evaluation:")
-        b_scores_cache = []
-        # print bootstrapped accuracy CIs
-        for out_folder in out_folders:
-            y_pred = test_contrastive_pred_knn(
-                torch.load(out_folder / "train_embeds.pth"),
-                torch.load(out_folder / "test_embeds.pth"),
-                torch.load(out_folder / "train_labels.pth"),
-                torch.load(out_folder / "test_labels.pth"),
-                k
-            )
-            y_true = torch.load(out_folder / "test_labels.pth")
-            print(out_folder)
-            print("Means, 95% CI Low, 95% CI High")
-            metric_mean, ci_low, ci_high, b_scores = bootstrap_metric(y_pred, y_true, accuracy)
-            b_scores_cache.append(b_scores)
-            print(metric_mean, ci_low, ci_high)
-            print()
-        # print accuracy difference for each pair of models
-        for i in range(len(out_folders) // 2):
-            i1 = 2 * i
-            i2 = 2 * i + 1
-            print("Accuracy Difference 95% CI for:")
-            print(out_folders[i1])
-            print(out_folders[i2])
-            print(bootstrap_dif(b_scores_cache[i1], b_scores_cache[i2]))
-            print()
+        for model_folders in model_folders_group:
+            b_scores_cache = []
+            # print bootstrapped accuracy CIs
+            for out_folder in model_folders:
+                y_pred = test_contrastive_pred_knn(
+                    torch.load(out_folder / "train_embeds.pth"),
+                    torch.load(out_folder / "test_embeds.pth"),
+                    torch.load(out_folder / "train_labels.pth"),
+                    torch.load(out_folder / "test_labels.pth"),
+                    k
+                )
+                y_true = torch.load(out_folder / "test_labels.pth")
+                print(out_folder)
+                print("Means, 95% CI Low, 95% CI High")
+                metric_mean, ci_low, ci_high, b_scores = bootstrap_metric(y_pred, y_true, accuracy)
+                b_scores_cache.append(b_scores)
+                print(metric_mean, ci_low, ci_high)
+                print()
+            # print accuracy difference for each pair of models
+            for i in range(1, len(model_folders)):
+                for j in range(i):
+                    print("Accuracy Difference 95% CI for:")
+                    print(model_folders[j])
+                    print(model_folders[i])
+                    print(bootstrap_dif(b_scores_cache[j], b_scores_cache[i]))
+                    print()
