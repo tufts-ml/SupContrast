53e38c37d323dfae744644cd034664da60c18bf5
PatrickFeeney <patrickfeeney1225@gmail.com>
2025-01-24 08:57:43 -0500

    Comment Bootstrap Dif

--- corruption.py
+++ corruption.py
@@ -173,13 +173,13 @@ if __name__ == "__main__":
         if "cifar100_" in model_folders[0].name:
             torch.save(torch.Tensor(acc_cache), "cifar100c_acc.pth")
         # print accuracy difference for each pair of models
-        for i in range(1, len(model_folders)):
-            for j in range(i):
-                print("Accuracy Difference 95% CI for:")
-                print(model_folders[j])
-                print(torch.mean(torch.vstack(b_scores_cache[j])))
-                print(model_folders[i])
-                print(torch.mean(torch.vstack(b_scores_cache[i])))
-                print(bootstrap_dif(torch.vstack(b_scores_cache[j]),
-                                    torch.vstack(b_scores_cache[i])))
-                print()
+        # for i in range(1, len(model_folders)):
+        #     for j in range(i):
+        #         print("Accuracy Difference 95% CI for:")
+        #         print(model_folders[j])
+        #         print(torch.mean(torch.vstack(b_scores_cache[j])))
+        #         print(model_folders[i])
+        #         print(torch.mean(torch.vstack(b_scores_cache[i])))
+        #         print(bootstrap_dif(torch.vstack(b_scores_cache[j]),
+        #                             torch.vstack(b_scores_cache[i])))
+        #         print()
