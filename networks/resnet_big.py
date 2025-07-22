0eaa85311da34b32614d9e583befbf75d4bbd8c8
mlao01-x <lao.mengkeat@gmail.com>
2025-07-22 11:52:41 -0400

    Linear Classifer Feature Dimension

--- networks/resnet_big.py
+++ networks/resnet_big.py
@@ -216,9 +216,10 @@ class SupCEResNet(nn.Module):
 class LinearClassifier(nn.Module):
     """Linear classifier"""
 
-    def __init__(self, name='resnet50', num_classes=10):
+    def __init__(self, name='resnet50', num_classes=10, feat_dim=None):
         super(LinearClassifier, self).__init__()
-        _, feat_dim = model_dict[name]
+        if feat_dim is None:
+            _, feat_dim = model_dict[name]
         self.fc = nn.Linear(feat_dim, num_classes)
 
     def forward(self, features):
