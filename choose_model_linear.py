74d18b5390e163e5f55037aaadf0662a6696ec60
mlao01-x <lao.mengkeat@gmail.com>
2025-09-10 22:49:25 -0400

    Handle EpsSupInfoNCE

--- choose_model_linear.py
+++ choose_model_linear.py
@@ -2,6 +2,8 @@ import os
 import csv
 from pathlib import Path
 
+from tqdm import tqdm
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -12,11 +14,15 @@ from networks.resnet_big import SupConResNet, LinearClassifier
 
 class ChooseLinearModel:
     def __init__(
-        self, heads_dir, sincere_encoder_ckpt, supcon_encoder_ckpt, dataset_opt
+        self, heads_dir, sincere_encoder_ckpt, supcon_encoder_ckpt, dataset_opt, epssupinfonce_encoder_ckpt=None,
     ):
         self.heads_dir = Path(heads_dir)
         self.sincere_encoder_ckpt = Path(sincere_encoder_ckpt)
         self.supcon_encoder_ckpt = Path(supcon_encoder_ckpt)
+        self.epssupinfonce_encoder_ckpt = (
+            Path(epssupinfonce_encoder_ckpt) if epssupinfonce_encoder_ckpt else None
+        )
+
         self.opt = dataset_opt
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -59,16 +65,27 @@ class ChooseLinearModel:
             print("No valid head folders found.")
             return
 
-        for head_folder in self.head_folders:
+        progress_bar = tqdm(self.head_folders, desc="Model evaluation")
+        for head_folder in progress_bar:
             try:
                 head_path = head_folder / "last.pth"
 
-                if "SINCERE" in head_folder.name.upper():
+                if "SINCERE" in head_folder.name:
                     encoder_path = self.sincere_encoder_ckpt
                     encoder_type = "SINCERE"
+                elif "EpsSupInfoNCE" in head_folder.name:
+                    if not self.epssupinfonce_encoder_ckpt:
+                        print(
+                            f"Skipping {head_folder.name}: EpsSupInfoNCE checkpoint not provided."
+                        )
+                        continue
+                    encoder_path = self.epssupinfonce_encoder_ckpt
+                    encoder_type = "EpsSupInfoNCE"
                 else:
                     encoder_path = self.supcon_encoder_ckpt
                     encoder_type = "SupCon"
+
+                progress_bar.set_description(f"Processing {encoder_type}")
 
                 lr, decay = self.__get_run_details(head_folder.name)
 
@@ -98,37 +115,39 @@ class ChooseLinearModel:
 
         encoder = SupConResNet(name=self.opt["model"])
         ckpt = torch.load(encoder_path, map_location=self.device, weights_only=False)
-        sd = {k.replace("module.", ""): v for k, v in ckpt["model"].items()}
-        encoder.load_state_dict(sd, strict=False)
+        encoder.load_state_dict(ckpt["model"])
 
         classifier = LinearClassifier(
-            name=self.opt["model"], num_classes=self.opt["n_cls"]
+            name=self.opt["model"], num_classes=self.opt["n_cls"], feat_dim=128
         )
         classifier_ckpt = torch.load(head_path, map_location=self.device, weights_only=False)
         classifier.load_state_dict(classifier_ckpt["model"])
 
-        model = FullModel(encoder.encoder, classifier).to(self.device)
+        model = FullModel(encoder, classifier).to(self.device)
         model.eval()
 
-        total_loss, total_samples = 0, 0
+        all_losses_tensors = []
         with torch.no_grad():
             for images, labels in self.val_loader:
                 images, labels = images.to(self.device), labels.to(self.device)
-                bsz = labels.shape[0]
                 output = model(images)
-                loss = F.cross_entropy(output, labels)
-                total_loss += loss.item() * bsz
-                total_samples += bsz
-        avg_val_loss = total_loss / total_samples
+                losses = F.cross_entropy(output, labels, reduction='none')
+                all_losses_tensors.append(losses)
+
+        all_losses = torch.cat(all_losses_tensors)
+        avg_val_loss = all_losses.mean().item()
 
         correct, total = 0, 0
         with torch.no_grad():
             for images, labels in self.test_loader:
                 images, labels = images.to(self.device), labels.to(self.device)
                 output = model(images)
-                _, predicted = torch.max(output.data, 1)
+
+                predicted = output.argmax(dim=1, keepdim=True)
+        
+                correct += (predicted.squeeze(1) == labels).sum().item()
                 total += labels.size(0)
-                correct += (predicted == labels).sum().item()
+
         test_accuracy = correct / total
 
         return avg_val_loss, test_accuracy
@@ -168,27 +187,47 @@ class ChooseLinearModel:
 
 if __name__ == "__main__":
     heads_directory = (
-        "/cluster/tufts/hugheslab/mlao01/Git/SupContrast/save/linear/exp5/cifar10/size-32/resnet-18-cifar10-32x32-batch-1000/"
+        "/cluster/tufts/hugheslab/mlao01/Git/SupContrast/save/linear/cynthia/imagenet100/size-32/cynthia-imagenet100/25_epochs/"
     )
 
-    sincere_encoder_checkpoint = "/cluster/tufts/hugheslab/mlao01/Git/SupContrast/save/SupCon/exp5/resnet-18-cifar10-32x32-batch-1000/cifar10_models/SINCERE_cifar10_resnet18_lr_0.2859301839839105_decay_0.001_bsz_1000_temp_0.1_trial_0_cosine_warm_2025_07_24-02_32_48/last.pth"
-    supcon_encoder_checkpoint = "/cluster/tufts/hugheslab/mlao01/Git/SupContrast/save/SupCon/exp5/resnet-18-cifar10-32x32-batch-1000/cifar10_models/SupCon_cifar10_resnet18_lr_0.2859301839839105_decay_0.001_bsz_1000_temp_0.1_trial_0_cosine_warm_2025_07_24-02_38_26/last.pth"
+    # cynthia imagenet100
+    sincere_encoder_checkpoint = "/cluster/tufts/hugheslab/mlao01/Git/SupContrast/save/SupCon/cynthia/SINCERE_imagenet100_resnet50_lr_0.65_decay_0.0001_bsz_512_temp_0.05_trial_0_cosine_warm_2024_01_22-09_32_18/last.pth"
+    supcon_encoder_checkpoint = "/cluster/tufts/hugheslab/mlao01/Git/SupContrast/save/SupCon/cynthia/SupCon_imagenet100_resnet50_lr_0.5_decay_0.0001_bsz_512_temp_0.05_trial_0_cosine_warm_2024_01_22-09_32_20/last.pth"
+    epssupinfonce_encoder_checkpoint = "/cluster/tufts/hugheslab/mlao01/Git/SupContrast/save/SupCon/cynthia/EpsSupInfoNCE_imagenet100_resnet50_lr_0.5_decay_0.0001_bsz_512_temp_0.1_trial_0_cosine_warm_2024_03_22-09_31_46/last.pth"
 
+    # # cynthia cifar10
+    # sincere_encoder_checkpoint = "/cluster/tufts/hugheslab/mlao01/Git/SupContrast/save/SupCon/cynthia/cifar10_models/SINCERE_cifar10_resnet50_lr_0.65_decay_0.0001_bsz_512_temp_0.1_trial_0_cosine_warm_2024_01_20-22_04_43/last.pth"
+    # supcon_encoder_checkpoint = "/cluster/tufts/hugheslab/mlao01/Git/SupContrast/save/SupCon/cynthia/cifar10_models/SupCon_cifar10_resnet50_lr_0.35_decay_0.0001_bsz_512_temp_0.05_trial_0_cosine_warm_2024_01_19-15_04_54/last.pth"
+    # epssupinfonce_encoder_checkpoint = "/cluster/tufts/hugheslab/mlao01/Git/SupContrast/save/SupCon/cynthia/cifar10_models/EpsSupInfoNCE_cifar10_resnet50_lr_0.5_decay_0.0001_bsz_512_temp_0.1_trial_0_cosine_warm_2024_03_21-12_28_30/last.pth"
+
+    # imagenet100
     dataset_options = {
-        "dataset": "cifar10",
-        "model": "resnet18",
+        "dataset": "imagenet100",
+        "model": "resnet50",
         "size": 32,
         "batch_size": 512,
-        "num_workers": 2,
+        "num_workers": 4,
         "valid_split": 0.1,
-        "n_cls": 10,
+        "n_cls": 100,
     }
+
+    # # cifar10
+    # dataset_options = {
+    #     "dataset": "cifar10",
+    #     "model": "resnet50",
+    #     "size": 32,
+    #     "batch_size": 1024,
+    #     "num_workers": 8,
+    #     "valid_split": 0.1,
+    #     "n_cls": 10,
+    # }
 
     evaluator = ChooseLinearModel(
         heads_dir=heads_directory,
         sincere_encoder_ckpt=sincere_encoder_checkpoint,
         supcon_encoder_ckpt=supcon_encoder_checkpoint,
         dataset_opt=dataset_options,
+        epssupinfonce_encoder_ckpt=epssupinfonce_encoder_checkpoint,
     )
     evaluator.process_runs()
     evaluator.save_results_to_csv()
