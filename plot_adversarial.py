b65eb6a18a2bd2a59dbfff9c6e83d23ffa1ac585
PatrickFeeney <patrickfeeney1225@gmail.com>
2026-02-05 12:21:47 -0500

    Joint Figure with Shading

--- plot_adversarial.py
+++ plot_adversarial.py
@@ -22,6 +22,7 @@ def bootstrap_adv_acc_diff(acc_val_1, acc_val_2, dataset_size):
 
 
 if __name__ == "__main__":
+    datasets = ["cifar2", "cifar10", "cifar100", "imagenet100"]
     dataset_test_sizes = {
         "cifar2": 2000,
         "cifar10": 10000,
@@ -35,7 +36,22 @@ if __name__ == "__main__":
         "imagenet100": "ImageNet-100",
     }
     folder = Path("./results_attack_no_cache/")
-    for dataset in ["cifar2", "cifar10", "cifar100", "imagenet100"]:
+
+    # init figure
+    plt.figure(figsize=(16, 4))
+    plt.subplots_adjust(left=0.05,
+                        right=0.99,
+                        bottom=0.13,
+                        top=0.92)
+    plt.rcParams.update({'font.size': 16})
+    # subplot spanning all 4 without any components besides x label
+    ax = plt.subplot(1, 4, (1, 4))
+    ax.yaxis.set_visible(False)
+    plt.setp(ax.spines.values(), visible=False)
+    ax.tick_params(left=False, labelleft=False, bottom=False, labelbottom=False)
+    ax.set_xlabel(r"Perturbation $\epsilon$", labelpad=24)
+
+    for i, dataset in enumerate(datasets):
         df = pd.read_csv(folder / Path(f"attack_embedding_results_{dataset}.csv"))
         # accuracy diff bootstrap results
         diff_results = [bootstrap_adv_acc_diff(acc_val_1, acc_val_2, dataset_test_sizes[dataset])
@@ -49,11 +65,21 @@ if __name__ == "__main__":
         # make array include insignificance gaps
         last_true_ind = len(diff_sigs) - 1 - diff_sigs[::-1].argmax()
         diff_sigs[diff_sigs.argmax():last_true_ind] = True
+
+        # init subplot
+        ax = plt.subplot(1, 4, i + 1)
+        ax.set_title(dataset_titles[dataset])
         for method_name in ["SINCERE", "SupCon"]:
-            plt.plot(df[df["model_name"] == method_name]["epsilon"][diff_sigs],
-                     df[df["model_name"] == method_name]["accuracy"][diff_sigs],
-                     label=method_name,)
-            plt.title(dataset_titles[dataset])
-        plt.legend()
-        plt.savefig(folder / f"{dataset}.pdf")
-        plt.close()
+            ax.fill_between(df[df["model_name"] == method_name]["epsilon"],
+                            1,
+                            where=list(~diff_sigs),
+                            facecolor="lightgrey")
+            ax.plot(df[df["model_name"] == method_name]["epsilon"],
+                    df[df["model_name"] == method_name]["accuracy"],
+                    label=method_name,)
+        # add axes labels on left
+        if i == 0:
+            ax.set_ylabel("Accuracy")
+    plt.legend()
+    plt.savefig(folder / "adversarial.pdf")
+    plt.close()
